c21798d53026b3c8d804a7dabdb0bbace43ee6b7
DharmaHS <dharma.siemarga@gmail.com>
2022-12-23 15:30:12 +0700

    distance now working

--- distance/recv.py
+++ distance/recv.py
@@ -1,111 +1,58 @@
 import socket
-import struct
+import pyaudio
 import time
 import numpy as np
-import pyaudio
-import threading
 from scipy.fft import fft, fftfreq
+from scipy import signal
 from utils import *
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
+# from sklearn.cluster import KMeans
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 5000))
+s.listen(1)
+dist = []
 
-# beep beep receiver
+while len(dist) < 20:
+    client, addr = s.accept()
 
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(
+        width=2), channels=1, rate=RATE, input=True, output=True)
+    # idx = findNearest(fftfreq(CHUNK//10), FREQ)
+    startTime = 0
+    endTime = 0
+    OFFSET = 1715 - 150
+    while True:
+        data = np.frombuffer(stream.read(CHUNK, False), dtype=np.int16)
 
-class Recv:
-    def __init__(self):
-        self.s_recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s_recv.bind(('0.0.0.0', RECV_PORT))
+        f, t, Zxx = signal.stft(data, RATE, nperseg=NPERSEG, noverlap=0, boundary=None)
+        idx = findNearest(f, FREQ)
+        amp = np.abs(Zxx)
 
-        self.s_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if amp[idx].max() > 200:
+            endTime += np.where(amp[idx] > 100)[0][0] * NPERSEG
+            print(endTime)
+            # endTime = time.time()
+            t_diff = (endTime - startTime) / RATE
+            # t_diff = endTime - startTime
+            print("dist ", 343 * t_diff * 100)
+            x = 343 * t_diff * 100
+            #y = -156.91087048 + 0.72303024*x
+            dist.append(x)
+            break
+        elif endTime - startTime > 99999:
+            break
+        else:
+            endTime += len(data)
 
-        self.p_in = pyaudio.PyAudio()
-        self.p_out = pyaudio.PyAudio()
-        self.stream_in = None
-        self.stream_out = None
-
-        self.t_a1, self.t_a2, self.t_a3 = None, None, None
-        self.t_b1, self.t_b2, self.t_b3 = None, None, None
-
-        self.listenThread = threading.Thread(target=self.startListen)
-
-        self.sendThread = None
-
-    def startListen(self):
-        self.stream_in = self.p_in.open(
-            format=pyaudio.paInt16, channels=1, rate=RATE, input=True)
-
-        isA3 = True
-        A3Ended = False
-        while True:
-            data = self.stream_in.read(CHUNK, exception_on_overflow=False)
-            data = np.frombuffer(data, dtype=np.int16)
-            freq = findNearest(fftfreq(len(data), d=1 / RATE), FREQ)
-            data_fft = np.abs(fft(data)[freq])
-
-            #print(data_fft)
-
-            if data_fft.max() > 500:
-                if isA3:
-                    self.t_a3 = time.time()
-                    print("Received A3 at {}".format(self.t_a3))
-                    isA3 = False
-                elif A3Ended:
-                    self.t_b2 = time.time()
-                    print("Received B2 at {}".format(self.t_b2))
-                    break
-            elif isA3:
-                A3Ended = True
-
-        self.stream_in.stop_stream()
-        self.stream_in.close()
-        self.p_in.terminate()
-
-    def startSend(self, addr):
-        wav = (AMPLITUDE * np.sin(2 * np.pi * np.arange(SAMPLES)
-                                  * FREQ / RATE)).astype(np.int16)
-        self.stream_out = self.p_out.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True,
-                                          frames_per_buffer=CHUNK)
-        self.s_send.connect((addr, SEND_PORT))
-
-        self.t_b1 = time.time()
-        self.stream_out.write(wav)
-
-        self.stream_out.stop_stream()
-        self.stream_out.close()
-        self.p_out.terminate()
-
-    def start(self):
-        self.s_recv.listen(1)
-        s, addr = self.s_recv.accept()
-
-        self.t_a1 = time.time()
-
-        self.listenThread.start()
-
-        self.sendThread = threading.Thread(target=self.startSend, args=(addr[0],))
-        self.sendThread.start()
-
-        self.sendThread.join()
-        self.listenThread.join()
-
-        b_diff = s.recv(1024)
-        b_diff = struct.unpack('f', b_diff)[0]
-        print(b_diff)
-
-        c = 337 * 100
-        D = c / 2 * ((self.t_a3 - self.t_a1) - b_diff)
-
-        self.s_recv.close()
-        self.s_send.close()
-        return D
-
-
-# listen for connection on socket, when received connection, start timer
-if __name__ == '__main__':
-    d = []
-    for i in range(10):
-        receiver = Recv()
-        d.append(receiver.start())
-        time.sleep(0.5)
-    print(d)
-    print(sum(d) / len(d))
+    stream.stop_stream()
+    stream.close()
+    time.sleep(1)
+plt.hist(dist, bins=10)
+plt.show()
+print(np.mean(dist))
+# kmeans = KMeans(n_clusters=8).fit(np.array(dist).reshape(-1, 1)).cluster_centers_.reshape(-1)
+# print(kmeans.min())
+# print(kmeans)
--- distance/send.py
+++ distance/send.py
@@ -1,93 +1,28 @@
 # run this code as the sender
 # always run recv first
 
-import pyaudio
+
 import socket
-from scipy.fft import fft, fftfreq
-import time
-import threading
-import struct
+import numpy as np
+import pyaudio
 from utils import *
+import time
 
+p = pyaudio.PyAudio()
+stream = p.open(format=p.get_format_from_width(
+    width=2), channels=1, rate=RATE, output=True)
+signal = AMPLITUDE * np.sin(np.pi * 2 * FREQ *
+                            np.arange(0, 0.2, 1.0 / RATE))
+port = 5000
+host_ip = "192.168.0.156"
+while (1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Beep beep sender
-class Sender:
-    def __init__(self, addr):
-        self.s_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.addr = addr
+    s.connect((host_ip, port))
 
-        self.s_recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s_recv.bind(('0.0.0.0', SEND_PORT))
+    stream.write(np.int16(signal).tobytes())
+    time.sleep(2)
+stream.stop_stream()
+stream.close()
 
-        self.p_in = pyaudio.PyAudio()
-        self.p_out = pyaudio.PyAudio()
-        self.stream_in = None
-        self.stream_out = None
-
-        self.t_a1, self.t_a2, self.t_a3 = None, None, None
-        self.t_b1, self.t_b2, self.t_b3 = None, None, None
-
-        self.listenThread = threading.Thread(target=self.startListen)
-
-        self.sendThread = None
-
-    def startListen(self):
-        self.stream_in = self.p_in.open(
-            format=pyaudio.paInt16, channels=1, rate=RATE, input=True)
-
-        isA2 = True
-        A2Ended = False
-        while True:
-            data = self.stream_in.read(CHUNK, exception_on_overflow=False)
-            data = np.frombuffer(data, dtype=np.int16)
-            freq = findNearest(fftfreq(len(data), d=1 / RATE), FREQ)
-            data_fft = fft(data)[freq]
-
-            if data_fft.max() > 500:
-                if isA2:
-                    self.t_a2 = time.time()
-                    isA2 = False
-                elif A2Ended:
-                    self.t_b3 = time.time()
-                    break
-            elif isA2:
-                A2Ended = True
-
-        self.stream_in.stop_stream()
-        self.stream_in.close()
-        self.p_in.terminate()
-
-    def start(self):
-        self.s_send.connect((self.addr, RECV_PORT))
-        self.t_a1 = time.time()
-
-        wav = (AMPLITUDE * np.sin(2 * np.pi * np.arange(SAMPLES)
-                                  * FREQ / RATE)).astype(np.int16)
-        self.stream_out = self.p_out.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True,
-                                          frames_per_buffer=CHUNK)
-
-        self.listenThread.start()
-        self.stream_out.write(wav)
-
-        self.stream_out.stop_stream()
-        self.stream_out.close()
-        self.p_out.terminate()
-
-        self.s_recv.listen(1)
-        self.s_recv.accept()
-        self.t_b1 = time.time()
-
-        self.listenThread.join()
-
-        print(self.t_b3 - self.t_b1)
-        self.s_send.sendall(struct.pack('f', self.t_b3 - self.t_b1))
-
-        self.s_send.close()
-        self.s_recv.close()
-
-
-if __name__ == '__main__':
-    for i in range(10):
-        sender = Sender("192.168.0.156")
-        sender.start()
-        time.sleep(1)
+p.terminate()
